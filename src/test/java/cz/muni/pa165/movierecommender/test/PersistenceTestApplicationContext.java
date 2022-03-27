3ff6fd6c45e15499ded2c3a959e657a84564c716
Petr Šlézar <485504@mail.muni.cz>
2022-03-27 22:31:29 +0200

    Db setting corrected

--- src/test/java/cz/muni/pa165/movierecommender/test/PersistenceTestApplicationContext.java
+++ src/test/java/cz/muni/pa165/movierecommender/test/PersistenceTestApplicationContext.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
 import org.springframework.instrument.classloading.LoadTimeWeaver;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,6 +17,7 @@ import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -38,7 +42,7 @@ public class PersistenceTestApplicationContext {
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean jpaFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        jpaFactoryBean.setPersistenceUnitName("test"); // persistence unit name in persistence.xml
+        jpaFactoryBean.setDataSource(db()); // persistence unit name in persistence.xml
         jpaFactoryBean.setLoadTimeWeaver(instrumentationLoadTimeWeaver());
         jpaFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
         return jpaFactoryBean;
@@ -59,5 +63,12 @@ public class PersistenceTestApplicationContext {
         TransactionTemplate transaction = new TransactionTemplate(transactionManager());
         transaction.afterPropertiesSet();
         return transaction;
+    }
+
+    @Bean
+    public DataSource db() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.DERBY).build();
+        return db;
     }
 }

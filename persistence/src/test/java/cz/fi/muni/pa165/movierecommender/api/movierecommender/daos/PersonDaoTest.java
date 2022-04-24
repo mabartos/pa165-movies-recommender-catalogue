aa0406b465a38bf0696f80f144011e240bf4039d
Maxim Svistunov <makssvistunov@gmail.com>
2022-04-24 21:44:52 +0200

    Fix paths in imports

--- persistence/src/test/java/cz/fi/muni/pa165/movierecommender/api/movierecommender/daos/PersonDaoTest.java
+++ persistence/src/test/java/cz/fi/muni/pa165/movierecommender/api/movierecommender/daos/PersonDaoTest.java
@@ -1,6 +1,6 @@
 package cz.fi.muni.pa165.movierecommender.api.movierecommender.daos;
 
-import cz.fi.muni.pa165.movierecommender.PersistenceTestApplicationContext;
+import cz.fi.muni.pa165.movierecommender.api.movierecommender.PersistenceTestApplicationContext;
 import cz.fi.muni.pa165.movierecommender.persistence.enums.Genre;
 import cz.fi.muni.pa165.movierecommender.persistence.dao.PersonDao;
 import cz.fi.muni.pa165.movierecommender.persistence.entity.Movie;

f2801bc7cac7aed4da68c7f6fc51524e407954ad
Daniel Puchala <xpuchal1@fi.muni.cz>
2022-05-19 09:31:54 +0200

    feat: cors

--- core/src/main/java/cz/fi/muni/pa169/movierecommender/core/MovieRecommenderApplication.java
+++ core/src/main/java/cz/fi/muni/pa169/movierecommender/core/MovieRecommenderApplication.java
@@ -2,15 +2,33 @@ package cz.fi.muni.pa169.movierecommender.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@ComponentScan(basePackages = {"cz.fi.muni.pa165.movierecommender","cz.fi.muni.pa165.movierecommender.rest"})
+@ComponentScan(basePackages = {"cz.fi.muni.pa165.movierecommender", "cz.fi.muni.pa165.movierecommender.rest"})
 @EnableAutoConfiguration
 public class MovieRecommenderApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(MovieRecommenderApplication.class, args);
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+
+
     }
 }

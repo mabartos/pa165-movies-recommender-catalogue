73daab9d5e322def65cd42a518cf784d0012979c
Daniel Puchala <xpuchal1@fi.muni.cz>
2022-05-19 16:49:01 +0200

    feat: added button for switching between recommended movies and reviews

--- frontend/src/components/Movie.tsx
+++ frontend/src/components/Movie.tsx
@@ -1,8 +1,15 @@
 import Header from './Header';
 import MovieCard from './MovieCard';
 import { MovieCardMode } from '../models/types';
+import { useState } from 'react';
 
 export const Movie = () => {
+  const [ showReviews, setShowReviews ] = useState<boolean>(false);
+
+  const changeShowStatus = () => {
+    setShowReviews((prevState => !prevState))
+  }
+
   //TODO Tahanie informacii
   const movie = {
     movie: 'Doctor Strange in the Multiverse of Madness',
@@ -15,7 +22,7 @@ export const Movie = () => {
 
   const recommendedMovies = [
     {
-      name: 'Movie',  id: 1,
+      name: 'Movie', id: 1,
       poster: 'https://cdn.shopify.com/s/files/1/0057/3728/3618/products/0ef9608d2219e0695a1b79af254f6e44_480x.progressive.jpg?v=1573572669',
       duration: 120
     },
@@ -40,15 +47,15 @@ export const Movie = () => {
       duration: 120
     },
     {
-      name: 'Movie',  id: 6,
+      name: 'Movie', id: 6,
       poster: 'https://cdn.shopify.com/s/files/1/0057/3728/3618/products/0ef9608d2219e0695a1b79af254f6e44_480x.progressive.jpg?v=1573572669',
       duration: 120
     }
   ];
 
   function formatDuration(num: number) {
-    const hours = Math.floor(num / 60)
-    return hours ? (`${hours} hours ${num % 60} minutes`) : (`${num % 60} minutes`)
+    const hours = Math.floor(num / 60);
+    return hours ? (`${hours} hours ${num % 60} minutes`) : (`${num % 60} minutes`);
   }
 
   return (
@@ -58,7 +65,7 @@ export const Movie = () => {
         <div className="flex flex-col col-span-2 h-full">
           <div className="flex flex-row p-4 border-solid border-r-4 border-slate-900 bg-slate-300 h-full w-full">
             <div className="p-4 flex-shrink-0 h-full w-2/5">
-              <img className="h-full w-full" src={movie.poster} alt="Poster" />
+              <img className="h-full w-full" src={movie.poster} alt="Poster"/>
             </div>
             <div className="flex flex-col p-4">
               <p className="text-2xl font-bold">{movie.movie}</p>
@@ -66,14 +73,23 @@ export const Movie = () => {
               <p className="mt-auto"><b>Duration:</b> {formatDuration(movie.duration)}</p>
               <p className="text-xl"><b>Director:</b> {movie.director}</p>
               <p className="text-xl"><b>Actors:</b> {movie.actors.map((actor) => `${actor}, `)}</p>
+              <button className="w-max p-4 self-center bg-blue-600 border-2 rounded-3xl border-slate-900" onClick={changeShowStatus}>
+                {showReviews ? "Show recommended movies" : "Show reviews"}
+              </button>
             </div>
           </div>
 
         </div>
-        <div className="h-full">
+        <div className={`h-full ${showReviews && "hidden"}`}>
           <p className="text-2xl text-center font-bold">Recommended movies</p>
-          <div className="flex flex-row flex-wrap p-4 fle">
+          <div className="flex flex-row flex-wrap p-4">
             {recommendedMovies.map((movie) => <MovieCard key={movie.id} {...movie} mode={MovieCardMode.Recommend}/>)}
+          </div>
+        </div>
+        <div className={`h-full ${showReviews || "hidden"}`}>
+          <p className="text-2xl text-center font-bold">Reviews</p>
+          <div className="flex flex-row p-4">
+            //TODO After get reviews
           </div>
         </div>
 

7142dde210c16536a526d7b5783143db9e09120a
Daniel Puchala <xpuchal1@fi.muni.cz>
2022-05-19 15:16:39 +0200

    feat: page for nonexistent urls

---
+++ frontend/src/components/PageNotFound.tsx
@@ -0,0 +1,14 @@
+import Header from './Header';
+
+export const PageNotFound = () => {
+  return (
+    <>
+      <Header />
+      <div>
+        Page not found
+      </div>
+    </>
+  )
+}
+
+export default PageNotFound;
--- frontend/src/components/Pages.tsx
+++ frontend/src/components/Pages.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import Movie from './Movie';
+import PageNotFound from './PageNotFound';
 
 export const Pages = () => (
   <BrowserRouter>
     <Routes>
       <Route path="pa165" element={<LandingPage/>}/>
       <Route path="pa165/movie/:id" element={<Movie/>}/>
+      <Route path="*" element={<PageNotFound />}/>
     </Routes>
   </BrowserRouter>
 );

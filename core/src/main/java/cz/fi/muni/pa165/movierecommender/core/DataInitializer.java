8384d9b89de0f34df18a1af66063d342c5be5f48
Petr Šlézar <485504@mail.muni.cz>
2022-05-21 19:34:44 +0200

    Data init started

--- core/src/main/java/cz/fi/muni/pa165/movierecommender/core/DataInitializer.java
+++ core/src/main/java/cz/fi/muni/pa165/movierecommender/core/DataInitializer.java
@@ -3,6 +3,7 @@ package cz.fi.muni.pa165.movierecommender.core;
 import cz.fi.muni.pa165.movierecommender.persistence.dao.UserDao;
 import cz.fi.muni.pa165.movierecommender.persistence.entity.Movie;
 import cz.fi.muni.pa165.movierecommender.persistence.entity.User;
+import cz.fi.muni.pa165.movierecommender.persistence.enums.Genre;
 import cz.fi.muni.pa165.movierecommender.persistence.enums.UserType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,11 @@ import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Component
@@ -30,6 +36,22 @@ public class DataInitializer {
 
         User admin = new User("admin",passwordEncoder.encode("admin"),"admin@gmail.com", UserType.ADMIN);
         systemUserDao.create(admin);
+    }
+
+    @Transactional
+    public void createMovies(){
+
+        Movie shawshankRedemption = new Movie("The Shawshank Redemption",142,
+                "https://play-lh.googleusercontent.com/hqVkofgQryL02Z2g_rsKpu923e2IMr_bXH7mCG0D0QOTLWT_a5qpv4zq3XxJ3vpcFm8hNnVkeLR6KYyi4ss",
+                new HashSet<Genre>(List.of(Genre.CRIME,Genre.DRAMA)),
+                "Mladý bankovní manažer Andy Dufresne (Tim Robbins) je v roce 1947 odsouzen na doživotí za vraždu své " +
+                        "ženy a jejího milence, kterou nespáchal. Čeká ho trest v obávané věznici Shawshank. Andy se " +
+                        "zde sblíží s černochem Redem (Morgan Freeman), jenž je tu už dvacet let, a během dlouhé doby se" +
+                        " jim společně podaří dosáhnout zlepšení zdejších poměrů. Andy se dokonce stane strážcům i " +
+                        "nenáviděnému řediteli věznice nepostradatelný jako daňový a finanční poradce.",
+                1994, Collections.emptySet(),null,Collections.emptySet());
+
+
     }
 
 }

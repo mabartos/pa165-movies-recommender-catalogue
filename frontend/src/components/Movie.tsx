97438c799b2e3b07950881d3ef8318662f8e06ab
Daniel Puchala <xpuchal1@fi.muni.cz>
2022-05-22 18:25:26 +0200

    feat: review form showing only for logged in users

--- frontend/src/components/Movie.tsx
+++ frontend/src/components/Movie.tsx
@@ -1,12 +1,13 @@
 import Header from './Header';
 import MovieCard from './MovieCard';
 import { MovieCardMode } from '../models/types';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 import instance from '../models/axios';
 import useSWR from 'swr';
 import fetcher from '../models/fetcher';
+import { getUser } from '../services/auth';
 
 function formatDuration(num: number) {
   const hours = Math.floor(num / 60);
@@ -108,42 +109,44 @@ export const Movie = () => {
             //TODO After get reviews
 
           </div>
-          <form
-            onSubmit={handleSubmit(sendReview)}
-            className="flex flex-col p-2 m-2 mt-auto bg-slate-300 border-4 rounded-3xl border-slate-900"
-          >
-            <span className="text-2xl text-center font-bold">Write your review</span>
-            <label className="flex justify-between">
-              <span className="font-bold">Script: {getValues('script')}</span>
-              <input type="range" min={1} max={10} {...register('script', { required: true })}/>
-            </label>
-            <label className="flex justify-between">
-              <span className="font-bold">Idea: {getValues('idea')}</span>
-              <input type="range" min={1} max={10} {...register('idea', { required: true })}/>
-            </label>
-            <label className="flex justify-between">
-              <span className="font-bold">Visual edits: {getValues('visualsEdit')}</span>
-              <input type="range" min={1} max={10} {...register('visualsEdit', { required: true })}/>
-            </label>
-            <label className="flex justify-between">
-              <span className="font-bold">Music: {getValues('music')}</span>
-              <input type="range" min={1} max={10} {...register('music', { required: true })}/>
-            </label>
-            <label className="flex justify-between">
-              <span className="font-bold">Acting: {getValues('acting')}</span>
-              <input type="range" min={1} max={10} {...register('acting', { required: true })}/>
-            </label>
-            <label>
+          {getUser() &&
+            <form
+              onSubmit={handleSubmit(sendReview)}
+              className="flex flex-col p-2 m-2 mt-auto bg-slate-300 border-4 rounded-3xl border-slate-900"
+            >
+              <span className="text-2xl text-center font-bold">Write your review</span>
+              <label className="flex justify-between">
+                <span className="font-bold">Script: {getValues('script')}</span>
+                <input type="range" min={1} max={10} {...register('script', { required: true })}/>
+              </label>
+              <label className="flex justify-between">
+                <span className="font-bold">Idea: {getValues('idea')}</span>
+                <input type="range" min={1} max={10} {...register('idea', { required: true })}/>
+              </label>
+              <label className="flex justify-between">
+                <span className="font-bold">Visual edits: {getValues('visualsEdit')}</span>
+                <input type="range" min={1} max={10} {...register('visualsEdit', { required: true })}/>
+              </label>
+              <label className="flex justify-between">
+                <span className="font-bold">Music: {getValues('music')}</span>
+                <input type="range" min={1} max={10} {...register('music', { required: true })}/>
+              </label>
+              <label className="flex justify-between">
+                <span className="font-bold">Acting: {getValues('acting')}</span>
+                <input type="range" min={1} max={10} {...register('acting', { required: true })}/>
+              </label>
+              <label>
               <textarea
                 className="w-full border-2 border-slate-900"
                 {...register('text', { required: true, minLength: 50 })}
                 placeholder={"At least 50 characters."}
               />
-            </label>
-            <button type="submit" className="w-max p-2 self-center bg-blue-600 border-2 rounded-3xl border-slate-900">
-              Send review
-            </button>
-          </form>
+              </label>
+              <button type="submit" className="w-max p-2 self-center bg-blue-600 border-2 rounded-3xl border-slate-900">
+                Send review
+              </button>
+            </form>
+          }
         </div>
 
       </div>

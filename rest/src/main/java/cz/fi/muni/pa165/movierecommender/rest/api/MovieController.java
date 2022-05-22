268b697a994f1091e31a550af49d81c9b881b35c
Martin Bartoš <mabartos@redhat.com>
2022-05-22 15:47:45 +0200

    Add missing imports

--- rest/src/main/java/cz/fi/muni/pa165/movierecommender/rest/api/MovieController.java
+++ rest/src/main/java/cz/fi/muni/pa165/movierecommender/rest/api/MovieController.java
@@ -2,9 +2,15 @@ package cz.fi.muni.pa165.movierecommender.rest.api;
 
 import cz.fi.muni.pa165.movierecommender.api.dto.MovieDto;
 import cz.fi.muni.pa165.movierecommender.api.dto.ReviewDto;
+import cz.fi.muni.pa165.movierecommender.api.dto.create.MovieCreateDto;
+import cz.fi.muni.pa165.movierecommender.api.dto.update.MovieUpdateDto;
 import cz.fi.muni.pa165.movierecommender.rest.core.RoutesHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
--- rest/src/main/java/cz/fi/muni/pa165/movierecommender/rest/api/PersonControllerProvider.java
+++ rest/src/main/java/cz/fi/muni/pa165/movierecommender/rest/api/PersonControllerProvider.java
@@ -1,12 +1,18 @@
 package cz.fi.muni.pa165.movierecommender.rest.api;
 
 import cz.fi.muni.pa165.movierecommender.api.dto.PersonDto;
+import cz.fi.muni.pa165.movierecommender.api.dto.create.PersonCreateDto;
+import cz.fi.muni.pa165.movierecommender.api.dto.update.PersonUpdateDto;
 import cz.fi.muni.pa165.movierecommender.rest.core.RoutesHolder;
 import cz.fi.muni.pa165.movierecommender.api.facade.PersonFacade;
 import cz.fi.muni.pa165.movierecommender.service.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
